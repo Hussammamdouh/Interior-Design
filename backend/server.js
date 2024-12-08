228c2f046163c1965c09d9981f21acbc074ea946
Hussammamdouh <hossammamdouh05@gmail.com>
2024-12-08 19:27:18 +0200

    Second Commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // For parsing JSON payloads
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/inventory', require('./routes/inventoryRoutes'));
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode).json({ message: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
