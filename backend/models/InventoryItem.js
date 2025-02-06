115792961c90b49727784b8cf9fc756acd092522
Hussammamdouh <125016995+Hussammamdouh@users.noreply.github.com>
2025-02-06 14:38:29 +0200

    Update InventoryItem.js

--- backend/models/InventoryItem.js
+++ backend/models/InventoryItem.js
@@ -5,6 +5,7 @@ const InventoryItemSchema = mongoose.Schema(
     name: { type: String, required: true },
     type: { type: String, required: true },
     price: { type: Number, required: true },
+    size: { type: Number, required: true },
     modelPath: { type: String, required: true },
     filePath: { type: String }, // New field for uploaded file path
     description: { type: String },
